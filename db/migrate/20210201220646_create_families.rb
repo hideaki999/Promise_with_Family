23f00c0fc9795d5e0c599718f76379bb87bd818c
hideaki999 <61974678+hideaki999@users.noreply.github.com>
2021-02-02 07:13:04 +0900

    familiesテーブル作成

--- db/migrate/20210201220646_create_families.rb
+++ db/migrate/20210201220646_create_families.rb
@@ -1,7 +1,9 @@
 class CreateFamilies < ActiveRecord::Migration[6.1]
   def change
     create_table :families do |t|
-
+      t.string :name
+      t.integer :relationships_id
+      t.integer :user_id , null: false
       t.timestamps
     end
   end
