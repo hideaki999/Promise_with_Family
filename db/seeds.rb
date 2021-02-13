350f8a0fbc5dc2eaa90a95dc18829edf44ffa45e
hideaki999 <61974678+hideaki999@users.noreply.github.com>
2021-02-13 18:22:33 +0900

    seedデータ、relationshipsテーブルに家族name追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+relationships = ['妻','夫','息子','娘','おじいちゃん','おばあちゃん']
+
+6.times do |n|
+Relationship.create!(
+    name: "#{relationships[n]}"
+)
+end
