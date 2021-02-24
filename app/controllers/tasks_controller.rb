630e80b9c186762976269cadab5e7b6e8edf7286
hideaki999 <61974678+hideaki999@users.noreply.github.com>
2021-02-24 19:14:34 +0900

    エラーメッセージデザイン,ログイン者メッセージ設定

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,13 +2,16 @@ class TasksController < ApplicationController
 
   def index
     @tasks = Task.all
+    @message = fight_message
   end
 
   def new
     @task = Task.new
+    @message = fight_message
   end
 
   def create
+    @message = fight_message
     @task = Task.new(task_prams)
     if @task.save
       redirect_to action: :index
@@ -18,6 +21,7 @@ class TasksController < ApplicationController
   end
 
   def edit
+    @message = fight_message
     @task = Task.find(params[:id])
   end
 
@@ -35,8 +39,16 @@ class TasksController < ApplicationController
 
 
   private
-
   def task_prams
     params.require(:task).permit(:title, :deadline_at, :families_id, :details).merge(user_id: 1)
+  end
+
+  def fight_message
+    message = ["今日も家族のために偉い！！！めっちゃ頑張ってるじゃん！",
+               "今日も家族の為にありがとう！家族は最高に喜んでるよ！！",
+               "一緒にやりましょう！せ～の (Ｉ・_・Ｉ) (Ｉ・_・Ｉ) (Ｉ・_・Ｉ) ヤァー！（だちょうクラブ）",
+               "今日も頑張ってるね！内田の兄貴と同じくらい頑張ってるよ！",
+               "(・∀・)ヽ耳がこう・・・(・∀・)３ゝでっかくなっちゃった！"]
+    message.sample
   end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -2,5 +2,6 @@ class Task < ApplicationRecord
     # belongs_to :user
 
     validates :title ,presence: true
+    validates :families_id ,presence: true
 
 end
